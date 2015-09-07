9e1b294857befd2f3f3978588518c712a02c7d82
Macquarie Sharpless <macquarie.sharpless@gmail.com>
2015-09-07 01:45:59 -0400

    Added Text#wrap_segments method

--- lib/ark/utility.rb
+++ lib/ark/utility.rb
@@ -90,15 +90,42 @@ module Ark
 
   # Methods for manipulating text
   module Text
+    
+    def self.wrap_segments(segments, width: 78, indent: 0, indent_after: false)
+      segments = segments.flatten.map(&:to_s)
+      lines = []
+      line = ''
+      current_indent = indent
+      segments.each do |seg|
+        if lines.empty? && indent_after
+          current_indent = 0
+        else
+          current_indent = indent
+        end
+        if line.size + seg.size >= width
+          lines << line
+          line = (' ' * indent) + seg
+        elsif line.empty?
+          line = (' ' * current_indent) + seg
+        else
+          line << ' ' << seg
+        end
+      end
+      lines << line if line
+      return lines.join("\n")
+    end
+
     # Wrap a string to a given width, with an optional indent. Indented text
     # will fall within the specified width.
     # [+text+] The text to be wrapped
     # [+width+] The number of columns to wrap within
     # [+indent+] Indent each wrapped line of +text+ by this number of columns
-    def self.wrap(text, width: 78, indent: 0)
-      width -= indent
-      text = text.gsub(/\s+/, " ").gsub(/(.{1,#{width}})( |\Z)/, "\\1\n")
-      text.gsub(/^/, ' ' * indent)
+    def self.wrap(text, width: 78, indent: 0, indent_after: false)
+      if text.is_a?(Array)
+        text = text.flatten.join(' ')
+      end
+      text = text.split(' ')
+      self.wrap_segments(text, width: width, indent: indent, indent_after: indent_after)
     end
   end
 
@@ -127,9 +154,12 @@ module Ark
 
     # Wrap the current line to +width+, with an optional +indent+. After
     # wrapping, the current line will be the last line wrapped.
-    def wrap(width: 78, indent: 0)
-      text = @lines[@line].join(' ')
-      text = Text.wrap(text, width: width, indent: indent)
+    def wrap(width: 78, indent: 0, indent_after: false, segments: false)
+      if segments
+        text = Text.wrap_segments(@lines[@line], width: width, indent: indent, indent_after: indent_after)
+      else
+        text = Text.wrap(@lines[@line], width: width, indent: indent, indent_after: indent_after)
+      end
       @lines.delete_at(@line)
       @line -= 1
       text.split("\n").each {|line| self.next(line) }
